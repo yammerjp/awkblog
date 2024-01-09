f83c4d4b4270f569537b53d1c05e126ff29948b4
Keisuke Nakayama <me@yammer.jp>
2024-01-10 05:15:13 +0900

    rm debug log

--- docker/nginx/start-nginx.sh
+++ docker/nginx/start-nginx.sh
@@ -3,6 +3,5 @@
 set -ex
 
 envsubst '$$PORT $$AWKBLOG_PORT $$AWKBLOG_INTERNAL_HOSTNAME'< /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
