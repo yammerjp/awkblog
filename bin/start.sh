dffece60416e4bb0df45153fa5f77b3fa9186aed
Keisuke Nakayama <me@yammer.jp>
2024-10-14 13:53:29 +0900

    WORKERS -> AWKBLOG_WORKERS

--- bin/start.sh
+++ bin/start.sh
@@ -4,18 +4,9 @@ set -e
 REPOSITORY_ROOT="$(dirname "$0")/.."
 cd "$REPOSITORY_ROOT"
 
-if [ "$WORKERS" == "" ]; then
-  WORKERS=1
+if [ "$AWKBLOG_WORKERS" == "" ]; then
+  AWKBLOG_WORKERS=1
 fi
-
-# function shutdown_trap() {
-#   echo "start.sh: shutdown_trap()"
-#   exit 100
-# }
-# 
-# trap shutdown_trap SIGTERM
-
-
 
 echo "start.sh: Build templates to src/_compiled_templates.awk"
 bin/compile_templates.sh
@@ -25,7 +16,7 @@ echo "start.sh: Migrate Database Schema"
 
 echo "start.sh: Start Web Application"
 
-for AWKBLOG_PORT in $(seq 40001 $((40000 + WORKERS))); do
+for AWKBLOG_PORT in $(seq 40001 $((40000 + AWKBLOG_WORKERS))); do
   cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 128 | AWKBLOG_PORT=$AWKBLOG_PORT gawk \
     $(find src/ -type f | gawk '/\.awk$/{ printf " -f %s", $0 }') \
     &
@@ -36,5 +27,3 @@ done
 echo "start.sh: built /etc/nginx/conf.d/default.conf"
 
 exec nginx -g "daemon off;"
-
-# pwait
